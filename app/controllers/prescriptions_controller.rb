af11d97be404d5db22fbdd50ab2b33c7ccd8a5e4
Shusaku Murata <murata.shayou@gmail.com>
2024-01-20 16:09:55 +0900

    一旦完成。後はDBに情報がない時にどうなるか確認

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,9 +1,9 @@
 class PrescriptionsController < ApplicationController
 	def show
 		@karte_history = KarteHistory.find(params[:id])
-		@prescriptions = @karte_history.karte_prescriptions
+		
 		@clinic_information = ClinicInformation.last
-		@patient_history = @karte_history.karte.patient.patient_histories.last
+		
 
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_110839) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_20_051304) do
   create_table "clinic_informations", force: :cascade do |t|
     t.string "name"
     t.string "postal_code"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_110839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "prescription_additional_comment"
+    t.date "prescription_limit_date"
   end
 
   create_table "karte_prescriptions", force: :cascade do |t|
